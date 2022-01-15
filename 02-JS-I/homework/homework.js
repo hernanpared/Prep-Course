3d07895b777310a4eaa909f33561785e69e9fe52
hernanpared <hernan2014@hotmail.com>
2022-01-15 12:29:41 -0300

    hora12:30

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -169,20 +169,21 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  return console.log(nombre + " " + apellido);
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  return console.log("Hola "+nombre+"!");
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  var area = (alto*ancho);
+  return area;
 }
 
 
@@ -214,6 +215,9 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+
+  if (letra==="a" || letra==="e" || letra==="i" || letra==="o" || letra==="u"){return console.log("Es vocal");} else {console.log("Dato incorrecto");}
+  
   
 }
 
